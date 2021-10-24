f47b53150f92eb570f376871feda1fd2dc140464
mochuvfx <87718949+mochuvfx@users.noreply.github.com>
2021-10-24 12:45:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+song1 = "";
+song2 = "";
+song1_status = "";
+song2_status = "";
+
+function preLoad() {
+    song = loadSound("music.mp3");
+}
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('poseNet is intialized');
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
